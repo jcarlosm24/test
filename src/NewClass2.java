edd029f9b5f270934d83f3c950c8c21049bd497b
jean <jean@jean-PC>
2015-07-11 21:57:31 -0430

    pruebas

---
+++ src/NewClass2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jean
+ */
+public class NewClass2 {
+  private String name;
+  private String jc;
+
+    public NewClass2(String name, String jc) {
+        this.name = name;
+        this.jc = jc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJc() {
+        return jc;
+    }
+
+    public void setJc(String jc) {
+        this.jc = jc;
+    }
+
+    public NewClass2() {
+    }
+  
+}
